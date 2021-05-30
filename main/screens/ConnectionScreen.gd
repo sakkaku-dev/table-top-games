70eb7b6fe84444161c0b316a6992fd863b9f3de6
myin <minyin142p@gmail.com>
2021-05-30 13:39:22 +0200

    use uuid for login

--- main/screens/ConnectionScreen.gd
+++ main/screens/ConnectionScreen.gd
@@ -1,47 +1,36 @@
+# Modified from original "Fish Game" code
+# To using single identifier for easier/simpler login
+
 extends "res://main/Screen.gd"
 
-onready var tab_container := $TabContainer
-onready var login_email_field := $TabContainer/Login/GridContainer/Email
-onready var login_password_field := $TabContainer/Login/GridContainer/Password
+onready var username_field := $Login/VBoxContainer/HBoxContainer/Username
 
-const CREDENTIALS_FILENAME = 'user://credentials.json'
+const ID_FILENAME = 'user://unique_id'
 
-var email: String = ''
-var password: String = ''
+var id: String = ''
+var username: String = ''
 
 var _reconnect: bool = false
 var _next_screen
 
 func _ready() -> void:
 	var file = File.new()
-	if file.file_exists(CREDENTIALS_FILENAME):
-		file.open(CREDENTIALS_FILENAME, File.READ)
-		var result := JSON.parse(file.get_as_text())
-		if result.result is Dictionary:
-			email = result.result['email']
-			password = result.result['password']
-			login_email_field.text = email
-			login_password_field.text = password
+	if file.file_exists(ID_FILENAME):
+		file.open(ID_FILENAME, File.READ)
+		id = file.get_line()
 		file.close()
 
 func _save_credentials() -> void:
 	var file = File.new()
-	file.open(CREDENTIALS_FILENAME, File.WRITE)
-	var credentials = {
-		email = email,
-		password = password,
-	}
-	file.store_line(JSON.print(credentials))
+	file.open(ID_FILENAME, File.WRITE)
+	file.store_line(id)
 	file.close()
 
 func _show_screen(info: Dictionary = {}) -> void:
 	_reconnect = info.get('reconnect', false)
 	_next_screen = info.get('next_screen', 'MatchScreen')
 	
-	tab_container.current_tab = 0
-	
-	# If we have a stored email and password, attempt to login straight away.
-	if email != '' and password != '':
+	if id != '':
 		do_login()
 
 func do_login(save_credentials: bool = false) -> void:
@@ -52,16 +41,11 @@ func do_login(save_credentials: bool = false) -> void:
 	else:
 		ui_layer.show_message("Logging in...")
 	
-	var nakama_session = yield(Online.nakama_client.authenticate_email_async(email, password, null, false), "completed")
+	var nakama_session = yield(Online.nakama_client.authenticate_device_async(id, username), "completed")
 	
 	if nakama_session.is_exception():
 		visible = true
 		ui_layer.show_message("Login failed!")
-		
-		# Clear stored email and password, but leave the fields alone so the
-		# user can attempt to correct them.
-		email = ''
-		password = ''
 		
 		# We always set Online.nakama_session in case something is yielding
 		# on the "session_changed" signal.
@@ -75,51 +59,16 @@ func do_login(save_credentials: bool = false) -> void:
 		if _next_screen:
 			ui_layer.show_screen(_next_screen)
 
-func _on_LoginButton_pressed() -> void:
-	email = login_email_field.text.strip_edges()
-	password = login_password_field.text.strip_edges()
-	do_login($TabContainer/Login/GridContainer/SaveCheckBox.pressed)
 
-func _on_CreateAccountButton_pressed() -> void:
-	email = $"TabContainer/Create Account/GridContainer/Email".text.strip_edges()
-	password = $"TabContainer/Create Account/GridContainer/Password".text.strip_edges()
+func _on_ConnectButton_pressed() -> void:
+	username = username_field.text.strip_edges()
 	
-	var username = $"TabContainer/Create Account/GridContainer/Username".text.strip_edges()
-	var save_credentials = $"TabContainer/Create Account/GridContainer/SaveCheckBox".pressed
-	
-	if email == '':
-		ui_layer.show_message("Must provide email")
-		return
-	if password == '':
-		ui_layer.show_message("Must provide password")
-		return
 	if username == '':
 		ui_layer.show_message("Must provide username")
 		return
 	
-	visible = false
-	ui_layer.show_message("Creating account...")
-
-	var nakama_session = yield(Online.nakama_client.authenticate_email_async(email, password, username, true), "completed")
+	var new_id = id == ''
+	if new_id:
+		id = UUID.v4()
 	
-	if nakama_session.is_exception():
-		visible = true
-		
-		var msg = nakama_session.get_exception().message
-		# Nakama treats registration as logging in, so this is what we get if the
-		# the email is already is use but the password is wrong.
-		if msg == 'Invalid credentials.':
-			msg = 'E-mail already in use.'
-		elif msg == '':
-			msg = "Unable to create account"
-		ui_layer.show_message(msg)
-		
-		# We always set Online.nakama_session in case something is yielding
-		# on the "session_changed" signal.
-		Online.nakama_session = null
-	else:
-		if save_credentials:
-			_save_credentials()
-		Online.nakama_session = nakama_session
-		ui_layer.hide_message()
-		ui_layer.show_screen("MatchScreen")
+	do_login(new_id)
