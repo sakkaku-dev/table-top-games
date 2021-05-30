afebfc23386373e985908fcb6cffa73fcbb05bb5
myin <minyin142p@gmail.com>
2021-05-30 13:10:04 +0200

    Copy code from fish game tutorial

--- screens/LobbyScreen.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-onready var username = $Username
-
-func _ready():
-	Online.connect("session_connected", self, "_set_user_info")
-
-func _set_user_info(session: NakamaSession):
-	username.text = session.username
